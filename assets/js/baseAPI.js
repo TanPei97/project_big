ba29b4f98f72a4a2692d4fa933e0f4472ca092f1
tanpei <tanpei@itcast.cn>
2020-08-16 16:55:20 +0800

    index后台页面基本信息完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,5 +1,16 @@
 $(function () {
     $.ajaxPrefilter(function (options) {
         options.url = 'http://ajax.frontend.itheima.net' + options.url;
+        if (options.url.indexOf('/my/')) {
+            options.headers = {
+                Authorization: sessionStorage.getItem('token') || ''
+            }
+        }
+        options.complete = function (res) {
+            if (res.responseJSON.status == 1 && res.responseJSON.message == '身份认证失败！') {
+                sessionStorage.removeItem('token');
+                location.href = '/login.html';
+            }
+        }
     })
 })
---
+++ assets/js/index.js
@@ -0,0 +1,34 @@
+$(function () {
+    getUsermsg();
+    // 退出
+    $('#logout').on('click', function () {
+        layer.confirm('是否确定退出？', { icon: 3, title: '提示' }, function (index) {
+            sessionStorage.removeItem('token');
+            location.href = '/login.html'
+            layer.close(index);
+        })
+    })
+})
+var layer = layui.layer;
+function getUsermsg() {
+    $.ajax({
+        method: 'get',
+        url: '/my/userinfo',
+        success: function (res) {
+            if (res.status !== 0) return layer.msg(res.message)
+            randerUsermsg(res.data)
+        }
+    })
+}
+function randerUsermsg(user) {
+    var name = user.nickname || user.username;
+    $('#welcome').html(name);
+    if (user.user_pic) {
+        $('.userImg').attr('src', user.user_pic).show();
+        $('.text-avatar').hide();
+    } else {
+        var text_avatar = name[0].toUpperCase();
+        $('.userImg').hide();
+        $('.text-avatar').html(text_avatar).show();
+    }
+}
